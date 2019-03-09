EtchedPixels/sdcc

commit 742c253b52bbabb7817be14d850657bb7c6dbe66

Improve code generation for 16-bit increment of global variables.
git-svn-id: http://svn.code.sf.net/p/sdcc/code/trunk@10627 4a8a32a2-be11-0410-ad9d-d568d2c75423

sdcc/src/z80/gen.c
@@ -4895,7 +4895,17 @@ genPlusIncr (const iCode * ic)
       while (icount--)
         emit2 ("inc %s", _pairs[pair].name);
       commitPair (AOP (IC_RESULT (ic)), pair, ic, FALSE);
-      return TRUE;
+      return true;
+    }
+
+  if (size == 2 && icount <= 2 && isPairDead (PAIR_HL, ic) &&
+    (IC_LEFT (ic)->aop->type == AOP_HL || IC_LEFT (ic)->aop->type == AOP_IY))
+    {
+      fetchPair (PAIR_HL, AOP (IC_LEFT (ic)));
+      emit2 ("inc hl");
+      regalloc_dry_run_cost++;
+      commitPair (AOP (IC_RESULT (ic)), PAIR_HL, ic, FALSE);
+      return true;
     }
 
   /* if increment 16 bits in register */

commit c83c8fc8b413b599ec17b0a1a3822775e9244f38

Fix upstream bug introduced in 742c253 / r10627, to generate correct code for increment by 2 or more in a particular special case handled by this optimization

sdcc/src/z80/gen.c
@@ -5340,7 +5340,8 @@ genPlusIncr (const iCode * ic)
     (IC_LEFT (ic)->aop->type == AOP_HL || IC_LEFT (ic)->aop->type == AOP_IY))
     {
       fetchPair (PAIR_HL, AOP (IC_LEFT (ic)));
-      emit2 ("inc hl");
+      while (icount--)
+        emit2 ("inc hl");
       regalloc_dry_run_cost++;
       commitPair (AOP (IC_RESULT (ic)), PAIR_HL, ic, FALSE);
       return true;